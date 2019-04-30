21441801b0ad6bea9650b3954a63f00b37180e5d
chathurangasandun <csk@tiqri.com>
2019-04-30 11:32:42 +0300

    custom badrequest error handing using automatic InvalidModelStateResponseFactory delegate

---
+++ PureModelValidation/Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PureModelValidation.Model;
+
+namespace PureModelValidation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("400");
+            }
+
+            return Ok(true);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ PureModelValidation/Startup.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace PureModelValidation
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                .ConfigureApiBehaviorOptions(options =>
+                {
+                    options.InvalidModelStateResponseFactory = context =>
+                    {
+                        var problems = new CustomBadRequest(context);
+
+                        return new BadRequestObjectResult(problems);
+                    };
+                });
+
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+
+    public class CustomBadRequest : ValidationProblemDetails
+    {
+        public CustomBadRequest(ActionContext context)
+        {
+            Title = "Invalid arguments to the API";
+            Detail = "The inputs supplied to the API are invalid";
+            Status = 400;
+            ConstructErrorMessages(context);
+            Type = context.HttpContext.TraceIdentifier;
+        }
+
+        private void ConstructErrorMessages(ActionContext context)
+        {
+            foreach (var keyModelStatePair in context.ModelState)
+            {
+                var key = keyModelStatePair.Key;
+                var errors = keyModelStatePair.Value.Errors;
+                if (errors != null && errors.Count > 0)
+                {
+                    if (errors.Count == 1)
+                    {
+                        var errorMessage = GetErrorMessage(errors[0]);
+                        Errors.Add(key, new[] { errorMessage });
+                    }
+                    else
+                    {
+                        var errorMessages = new string[errors.Count];
+                        for (var i = 0; i < errors.Count; i++)
+                        {
+                            errorMessages[i] = GetErrorMessage(errors[i]);
+                        }
+
+                        Errors.Add(key, errorMessages);
+                    }
+                }
+            }
+        }
+
+        string GetErrorMessage(ModelError error)
+        {
+            return string.IsNullOrEmpty(error.ErrorMessage) ?
+                "The input was not valid.":
+            error.ErrorMessage;
+        }
+    }
+}
